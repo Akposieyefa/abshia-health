2a16dfae01a1035f6b2dfcd8dc22a97647c05067
Akposieyefa <orutu1@gmail.com>
2022-06-15 03:06:05 +0100

    seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'email' => $this->faker->unique()->safeEmail(),
+            'email' => 'superadmin@gmail.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'role' => RolesEnum::SUPERADMIN,
